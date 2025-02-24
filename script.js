4c2b5a537fde35fe57ea37b8cf1eb61a948037f0
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-02-24 17:47:58 +0100

    odoeter feature for the numbers

--- script.js
+++ script.js
@@ -6,3 +6,45 @@ function toggleMenu() {
 		navbar.classList.add('active');
 	}
 }
+
+const createOdometer = (el, value) => {
+	const odometer = new Odometer ({
+		el: el,
+		value : 0,
+	});
+
+	let hasRun = false;
+
+	const options = {
+		threshold: [0, 0.9],
+	};
+
+	const callback = (entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry,isIntersecting) {
+				if (!hasRun)
+				{
+					odometer.update(value);
+					hasRun = true;
+				}
+			}
+		});
+	};
+
+	//Intersection Observer to make the counter load according to the viewport - otherwise it would have finished the counter by the time we scroll down
+	const observer = new IntersectionObserver(callback, options);
+	observer.observe(el);
+
+};
+
+const projectsOdometer = document.querySelector(".projects-odometer");
+createOdometer(projectsOdometer, 4452);
+
+const clientOdometer = document.querySelector(".client-odometer");
+createOdometer(clientOdometer, 1458);
+
+const serviceOdometer = document.querySelector(".service-odometer");
+createOdometer(serviceOdometer, 100);
+
+const teamOdometer = document.querySelector(".team-odometer");
+createOdometer(teamOdometer, 85);
